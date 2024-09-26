9dca5380ef44e6958f03bd16cfd51ab132fc594b
Octabiancotti <octaviobiancotti13@gmail.com>
2024-09-26 13:08:43 -0300

    plataforma de bienestar digital
    subiendo archivo

---
+++ TP3/plataforma_de_bienestar_digital/src/test/java/com/plataformabienestar/plataforma_de_bienestar_digital/PlataformaDeBienestarDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plataformabienestar.plataforma_de_bienestar_digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaDeBienestarDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
